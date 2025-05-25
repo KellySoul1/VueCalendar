90ba6a2626b54b5acf01e8c0b3dccf066b421a88
KellySoul1 <namdanil443@gmail.com>
2025-05-25 18:39:01 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-// Отключаем сообщения о продакшн режиме
+ 
 Vue.config.productionTip = false
 
-// Создаем и монтируем корневой экземпляр Vue
+ 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
